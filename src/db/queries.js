6c832525108313786047152ef3e8f174a18c31a7
udeaghad <dozie.udeagha@gmail.com>
2022-11-02 11:33:27 +0100

    Setup query for delete todo

--- src/db/queries.js
+++ src/db/queries.js
@@ -2,11 +2,13 @@ const addTodo = "INSERT INTO todos(description) VALUES ($1) RETURNING *"
 const getAllTodos = "SELECT * FROM todos"
 const getTodoItem = "SELECT * FROM todos WHERE id = $1"
 const updateItem = "UPDATE todos SET completed = $1 WHERE id = $2 RETURNING *"
+const deleteItem = "DELETE from todos WHERE id = $1"
 
 
 module.exports = { 
   addTodo,
   getAllTodos,
   getTodoItem,
-  updateItem
+  updateItem,
+  deleteItem
 }

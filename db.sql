809bfa87dac420d8a87ffcb9236d595f7e263894
udeaghad <dozie.udeagha@gmail.com>
2022-11-02 09:34:25 +0100

    Create database

---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE todo
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  description VARCHAR(255)
+)
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
 
 app.listen(5000, () => {
   console.log("Server is listening to port 5000...")

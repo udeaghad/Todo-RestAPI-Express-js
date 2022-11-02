97e30a7542282b11378d1750131373d4cd986568
udeaghad <dozie.udeagha@gmail.com>
2022-11-02 10:55:18 +0100

    Setup query to get todos item

--- src/db/queries.js
+++ src/db/queries.js
@@ -1,5 +1,6 @@
 const addTodo = "INSERT INTO todos(description) VALUES ($1) RETURNING *"
 const getAllTodos = "SELECT * FROM todos"
+const getTodoItem = "SELECT * FROM todos WHERE id = $1"
 
 
 module.exports = { 
